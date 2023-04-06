d9313c39a1af5adf1c0465a9293fc34c7c23f4b3
Louie Marcou <58005859+LouieMarcou@users.noreply.github.com>
2023-04-05 21:48:53 -0500

    updated

--- Assets/Scripts/Output/IOutputCreator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IOutputCreator<T>
-{
-    T OutputImage { get; }
-    void CreateOutput(PatternManager manager, int[][] outputValues, int width, int height);
-}
--- Assets/Scripts/ProceduralGeneration/WaveFunctionCollapse/IValue.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IValue<T> : IEqualityComparer<IValue<T>>, IEquatable<IValue<T>>
-{
-    T Value { get; }
-}
