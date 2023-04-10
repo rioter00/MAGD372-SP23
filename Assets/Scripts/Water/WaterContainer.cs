0e79d6b5352e6d8004df69457c5d5d6b2db89eb0
Lucas Ackman <AckmanLD@gmail.com>
2023-04-10 12:10:32 -0500

    Added Container and Faucet

---
+++ Assets/Scripts/Water/WaterContainer.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using Essentials.Reference_Variables.References;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class WaterContainer : MonoBehaviour
+{
+    [SerializeField] private FloatReference waterReference;
+    [SerializeField] private FloatReference maxFillAmountReference;
+
+    private float water
+    {
+        get
+        {
+            return waterReference.Value;
+        }
+        set
+        {
+            waterReference.Value = value;
+        }
+    }
+    private float maxFillAmount
+    {
+        get
+        {
+            return maxFillAmountReference.Value;
+        }
+    }
+    private float fillPerSecond;
+
+    private Coroutine waterCollection;
+
+    private IEnumerator CollectWater()
+    {
+        for (;;)
+        {
+            if (water < maxFillAmount)
+                water += fillPerSecond * Time.deltaTime;
+            else
+                water = maxFillAmount;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var faucet = other.gameObject.GetComponent<Faucet>();
+        if (!faucet)
+            return;
+
+        fillPerSecond = faucet.FillPerSecond;
+        waterCollection = StartCoroutine(CollectWater());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StopCoroutine(waterCollection);
+    }
+}
