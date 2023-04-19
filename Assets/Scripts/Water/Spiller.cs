41d637eb91b9142a2825aa904e2b6fade59c2e93
Lucas Ackman <AckmanLD@gmail.com>
2023-04-19 16:21:50 -0500

    Update Spiller.cs

--- Assets/Scripts/Water/Spiller.cs
+++ Assets/Scripts/Water/Spiller.cs
@@ -1,10 +1,8 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Spiller : MonoBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.TryGetComponent<Cup>(out var cup);
