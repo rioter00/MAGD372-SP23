a3628cf98a7d50f4dcb2b4b9db4349a3810af057
ZclipseCode <bmichaelbusiness@gmail.com>
2023-04-24 15:27:15 -0500

    events and delegation

--- Assets/Scripts/HazardsAndItems/Items/TempActivateItem.cs
+++ Assets/Scripts/HazardsAndItems/Items/TempActivateItem.cs
@@ -3,15 +3,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Essentials.Reference_Variables.Variables;
 
 public class TempActivateItem : MonoBehaviour
 {
     bool activated;
-    Item item;
+    //Item item;
+    [SerializeField] FloatVariable activationInput;
 
-    void Update()
+    //void Update()
+    //{
+    //    if (!activated && Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        item = GetComponent<Item>();
+    //        if (item != null)
+    //        {
+    //            item.Activate();
+    //            activated = true;
+    //        }
+    //    }
+
+    //    if (item == null)
+    //        activated = false;
+    //}
+
+    private void OnEnable()
     {
-        if (!activated && Input.GetKeyDown(KeyCode.E))
+        activationInput.ValueChanged += ItemActivation;
+    }
+
+    private void OnDisable()
+    {
+        activationInput.ValueChanged -= ItemActivation;
+    }
+
+    private void ItemActivation(object sender, System.EventArgs e)
+    {
+        if (activationInput.Value != 1)
+        {
+            return;
+        }
+
+        Item item = null;
+
+        if (!activated)
         {
             item = GetComponent<Item>();
             if (item != null)
