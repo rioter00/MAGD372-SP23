0a5096a30f2a3ac3bd45851685e54abeeb397079
ZclipseCode <bmichaelbusiness@gmail.com>
2023-04-26 15:29:22 -0500

    tumbleweed movement

--- Assets/Scripts/HazardsAndItems/Hazards/Tumbleweed.cs
+++ Assets/Scripts/HazardsAndItems/Hazards/Tumbleweed.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEditorInternal;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class Tumbleweed : MonoBehaviour
 {
+    [SerializeField] Transform contactPoint1;
+    [SerializeField] Transform contactPoint2;
     [SerializeField] float pushForce;
+    [SerializeField] float speed;
+    [SerializeField] float rotationFactor;
+    Vector3 cp1;
+    Vector3 cp2;
+    bool toContactPoint1 = true;
+    float initialY;
+
+    private void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        cp1 = new Vector3(contactPoint1.position.x, initialY, contactPoint1.position.z);
+        cp2 = new Vector3(contactPoint2.position.x, initialY, contactPoint2.position.z);
+
+        if (toContactPoint1)
+        {
+            transform.position = new Vector3(transform.position.x, initialY, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, cp1, Time.deltaTime * speed);
+            transform.Rotate(0, rotationFactor, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, initialY, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, cp2, Time.deltaTime * speed);
+            transform.Rotate(0, rotationFactor, 0);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,6 +47,11 @@ public class Tumbleweed : MonoBehaviour
         {
             Rigidbody player = collision.collider.GetComponent<Rigidbody>();
             Push(player);
+        }
+
+        if (collision.collider.CompareTag("Wall"))
+        {
+            toContactPoint1 = !toContactPoint1;
         }
     }
 
