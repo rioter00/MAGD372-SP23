736daafb7ebb0db833891c70377d51fc3110f8cf
staddlern <staddlern@gmail.com>
2023-04-26 14:45:55 -0500

    Testing Multiplayer Input

--- Assets/Scripts/PlayerInput/PlayerController.cs
+++ Assets/Scripts/PlayerInput/PlayerController.cs
@@ -6,71 +6,172 @@ using Essentials.Reference_Variables.Variables;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Vector2Variable move_input;
-    [SerializeField] private Vector2Variable camera_input;
-    [SerializeField] private FloatVariable jump_input;
-    [SerializeField] private FloatVariable interact_input;
-    [SerializeField] private FloatVariable shove_input;
-    [SerializeField] private FloatVariable spill_input;
-    [SerializeField] private FloatVariable powerup_one_input;
-    [SerializeField] private FloatVariable powerup_two_input;
-    [SerializeField] private FloatVariable pause_input;
-    [SerializeField] bool flipped;
+    [SerializeField] private InputValues player_one;
+    [SerializeField] private InputValues player_two;
+
+    [System.Serializable]
+    public class InputValues
+    {
+        [SerializeField] private Vector2Variable move_input;
+        [SerializeField] private Vector2Variable camera_input;
+        [SerializeField] private FloatVariable jump_input;
+        [SerializeField] private FloatVariable interact_input;
+        [SerializeField] private FloatVariable shove_input;
+        [SerializeField] private FloatVariable spill_input;
+        [SerializeField] private FloatVariable powerup_one_input;
+        [SerializeField] private FloatVariable powerup_two_input;
+        [SerializeField] private FloatVariable pause_input;
+        [SerializeField] bool flipped;
+
+        public Vector2 Move
+        {
+            set
+            {
+                if (flipped)
+                {
+                    value.x = -value.x;
+                }
+                move_input.Value = value;
+            }
+        }
+
+        public Vector2 Camera
+        {
+            set
+            {
+                if (flipped)
+                {
+                    value.x = -value.x;
+                }
+                camera_input.Value = value;
+            }
+        }
+
+        public float Jump
+        {
+            set
+            {
+                jump_input.Value = value;
+            }
+        }
+
+        public float Interact
+        {
+            set
+            {
+                interact_input.Value = value;
+            }
+        }
+
+        public float Shove
+        {
+            set
+            {
+                shove_input.Value = value;
+            }
+        }
+
+        public float Spill
+        {
+            set
+            {
+                spill_input.Value = value;
+            }
+        }
+
+        public float PowerupOne
+        {
+            set
+            {
+                powerup_one_input.Value = value;
+            }
+        }
+
+        public float PowerupTwo
+        {
+            set
+            {
+                powerup_two_input.Value = value;
+            }
+        }
+
+        public float Pause
+        {
+            set
+            {
+                pause_input.Value = value;
+            }
+        }
+    }
+
+    private Gamepad player1;
+    private Gamepad player2;
+
+    private void Start()
+    {
+        foreach (var gamepad in Gamepad.all)
+        {
+            if (player1 == null)
+            {
+                player1 = gamepad;
+            }
+            else
+            {
+                player2 = gamepad;
+            }
+        }
+    }
 
     public void PlayerMove(InputAction.CallbackContext context)
     {
-        Vector2 temp = context.ReadValue<Vector2>();
-        if (flipped)
+        InputDevice device = context.control.device;
+        Vector2 input = context.ReadValue<Vector2>();
+        if (device == player1)
         {
-            temp.x = -temp.x;
+            player_one.Move = input;
         }
-        move_input.Value = temp;
-        
+        else if (device == player2) {
+            player_two.Move = input;
+        }
     }
 
     public void PlayerCamera(InputAction.CallbackContext context)
     {
 
-        Vector2 temp = context.ReadValue<Vector2>();
-        if (flipped)
-        {
-            temp.x = -temp.x;
-        }
-        camera_input.Value = temp;
     }
 
     public void PlayerJump(InputAction.CallbackContext context)
     {
-        jump_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerInteract(InputAction.CallbackContext context)
     {
-        interact_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerShove(InputAction.CallbackContext context)
     {
-        shove_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerSpill(InputAction.CallbackContext context)
     {
-        spill_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerPowerupOne(InputAction.CallbackContext context)
     {
-        powerup_one_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerPowerupTwo(InputAction.CallbackContext context)
     {
-        powerup_two_input.Value = context.ReadValue<float>();
+        
     }
 
     public void PlayerPause(InputAction.CallbackContext context)
     {
-        pause_input.Value = context.ReadValue<float>();
+        
     }
 }
