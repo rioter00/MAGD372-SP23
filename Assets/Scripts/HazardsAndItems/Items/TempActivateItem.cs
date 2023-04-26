522c1caac60d5d964f2d3e32195f2df9952ead9d
ZclipseCode <bmichaelbusiness@gmail.com>
2023-04-26 14:47:30 -0500

    tumble work

--- Assets/Scripts/HazardsAndItems/Items/TempActivateItem.cs
+++ Assets/Scripts/HazardsAndItems/Items/TempActivateItem.cs
@@ -8,45 +8,12 @@ using Essentials.Reference_Variables.Variables;
 public class TempActivateItem : MonoBehaviour
 {
     bool activated;
-    //Item item;
-    [SerializeField] FloatVariable activationInput;
+    Item item = null; // remove this
+    //[SerializeField] FloatVariable activationInput; // add this
 
-    //void Update()
-    //{
-    //    if (!activated && Input.GetKeyDown(KeyCode.E))
-    //    {
-    //        item = GetComponent<Item>();
-    //        if (item != null)
-    //        {
-    //            item.Activate();
-    //            activated = true;
-    //        }
-    //    }
-
-    //    if (item == null)
-    //        activated = false;
-    //}
-
-    private void OnEnable()
+    void Update() // remove this
     {
-        activationInput.ValueChanged += ItemActivation;
-    }
-
-    private void OnDisable()
-    {
-        activationInput.ValueChanged -= ItemActivation;
-    }
-
-    private void ItemActivation(object sender, System.EventArgs e)
-    {
-        if (activationInput.Value != 1)
-        {
-            return;
-        }
-
-        Item item = null;
-
-        if (!activated)
+        if (!activated && Input.GetKeyDown(KeyCode.E))
         {
             item = GetComponent<Item>();
             if (item != null)
@@ -59,4 +26,39 @@ public class TempActivateItem : MonoBehaviour
         if (item == null)
             activated = false;
     }
+
+    // add everything underneath
+
+    //private void OnEnable()
+    //{
+    //    activationInput.ValueChanged += ItemActivation;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    activationInput.ValueChanged -= ItemActivation;
+    //}
+
+    //private void ItemActivation(object sender, System.EventArgs e)
+    //{
+    //    if (activationInput.Value != 1)
+    //    {
+    //        return;
+    //    }
+
+    //    Item item = null;
+
+    //    if (!activated)
+    //    {
+    //        item = GetComponent<Item>();
+    //        if (item != null)
+    //        {
+    //            item.Activate();
+    //            activated = true;
+    //        }
+    //    }
+
+    //    if (item == null)
+    //        activated = false;
+    //}
 }
