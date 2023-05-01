ed628c4f547a53826964bb27aca3d75dfeb50be6
NV-Navnit <navnitvenkatesh@gmail.com>
2023-05-01 14:24:49 -0500

    Adding More Effects
    Added 3D visuals to the Player Sand Trail, Sand Cloud, and Water Effect. Also added a Readme for some simple instructions

---
+++ Assets/Visual Effects/#README EFFECTS.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*These are instructions on the effects here and their intended uses. This script
+also explains how to use them or change some of their properties.*/
+
+//THIS IS STILL UNFINISHED
+
+//Pickup Effect/Backlight/Pickup Effect W/O Backlight
+
+/*This is the Effect meant as a background for pickups. There are two parts to this:
+the backlight and the trails. Some are split up in case the backlight or trails are undesired
+
+TrailSpawnRate: How many trails spawn at a time
+TrailLifetime: How long the trails last. 0.2 is recommended, higher numbers will make the trails "cave" into itself
+Color: Color of the trails and backlight. The trails are more affected by this
+Size: Size of the backlight and trails, its essentially a sphere*/
+
+//3D Player Sand Trail
+
+//A trail meant to go behind the player when running on sand. Needs a script in order to activate it. 
+
+//3D Sand Cloud 
+
+//A dust cloud that appears whenever the player lands in sand
+
+//3D Water Effect
+
+//The effect that comes from the well pipes that dispenses water.
+
